e9dc009c1aa7d8203927630a64c2756683f48ca0
swapnil-jadhav-2600 <75872067+swapnil-jadhav-2600@users.noreply.github.com>
2020-12-17 20:10:55 +0530

    done

--- ananomous fun.py
+++ ananomous fun.py
@@ -14,3 +14,24 @@ def add():
 
 a = add()
 print(a(10))
+
+#map, filter and reduce function
+#  filter : returns filtered values from  a sequence 
+lis = [1,2,3,4,5,6,7,8,9,10]
+final_lis = list(filter ((lambda a : (a%2!=0)) , lis))
+print(final_lis)
+
+# map : 
+#l1 = [1,2,3,4,5,6,7,8,9,10]
+#fl = list( map(lambda x: (x+2) , 1)
+
+# reduce 
+from functools import reduce
+li = [5,8,6,9,8]
+sum = reduce((lambda b,c:b+c),li)
+print(sum)
+
+li = [1,2,3,4,5,6]
+print('The maximum element in the list')
+print(reduce(lambda a,b: (a if a>b else b) , li))
+
